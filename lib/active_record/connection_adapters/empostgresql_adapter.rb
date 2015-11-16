bf24d2d13a684a622d6a015610651219e82c38fa
DanBradbury <dan.luckydaisy@gmail.com>
2015-11-16 01:05:41 -0800

    remove pg dependency

--- lib/active_record/connection_adapters/empostgresql_adapter.rb
+++ lib/active_record/connection_adapters/empostgresql_adapter.rb
@@ -1,18 +1,16 @@
-gem 'pg'
-require 'pg'
-
 require 'active_record/connection_adapters/postgresql_adapter'
 require 'active_record/connection_adapters/statement_pool'
 
 module ActiveRecord
-  module ConnectionHandling
-    def empostgresql_connection(config)
-    end
-  end
-
   module ConnectionAdapters
     class EMPostgreSQLAdapter < PostgreSQLAdapter
-      ADAPTER_NAME = "EMPostgreSQL".freeze
+      def initialize
+        super
+      end
+
+      def adapter_name
+        "EMpg".freeze
+      end
     end
   end
 end
